875a1133d2e2caecf7f7cc7a65203e8aae796d52
Georg A. Seyerl <g.seyerl@geoase.eu>
2020-07-02 13:12:23 +0200

    New tests, minor changes in methods# Please enter the commit message for your changes. Lines starting

--- cds_downloader/cds_downloader.py
+++ cds_downloader/cds_downloader.py
@@ -11,6 +11,7 @@ TODO:
  - Adapt requirements.txt
  - Dynamic split by lists in cds_filter (order) e.g.
    ensemble_member, experiment, model
+ - Check configuration
 """
 
 __author__ = "Georg Seyerl"
@@ -40,8 +41,6 @@ class ClimateDataStoreDownloader(object):
         self.cds_webapi = requests.get(
             url='https://cds.climate.copernicus.eu/api/v2.ui/resources/{}'.format(cds_product)).json()
 
-        # User Credentials from environment variables 'CDSAPI_URL' and 'CDSAPI_KEY'
-        self.cdsapi_client = cdsapi.Client()
 
     @classmethod
     def from_cds(cls, cds_product, cds_filter):
@@ -80,36 +79,44 @@ class ClimateDataStoreDownloader(object):
 
     def get_data(self, storage_path, split_keys=None):
         """Method documentation"""
+
+        # User Credentials from environment variables
+        # 'CDSAPI_URL' and 'CDSAPI_KEY'
+        self.cdsapi_client = cdsapi.Client()
+
         if split_keys is None:
             split_keys = self._get_split_keys()
 
         Path(storage_path).mkdir(parents=True, exist_ok=True)
 
         split_filter = self._expand_by_keys(self.cds_filter, split_keys)
+        all_processes = []
         for cds_filter in split_filter:
             file_path = '-'.join([cds_filter.get(k) for k in split_keys] or ["all"]) + \
                         "_" + self.cds_product + \
                         "." + cds_filter.get("format", "grib")
 
-            p = Process(
-                target=self._retrieve_file,
-                args=(self.cds_product,
-                        cds_filter,
-                        os.path.join(storage_path, file_path)
+            all_processes.append(
+                Process(
+                    target=self._retrieve_file,
+                    args=(self.cds_product,
+                          cds_filter,
+                          os.path.join(storage_path, file_path)
+                    )
                 )
             )
+
+        for p in all_processes:
             p.start()
+        for p in all_processes:
+            p.join()
 
+        return all_processes
 
-    def _get_split_keys(self, lst_keys = ["year", "month", "day", "time", "variable"]):
-        if self._get_request_size(lst_keys) < self.cds_webapi["selection_limit"]:
-            return list()
-        else:
-            k = lst_keys.pop(0)
-            split_keys = self._get_split_keys(lst_keys)
-            split_keys.insert(0, k)
-            return split_keys
-
+    def _get_org_keys(self):
+        exclude_keys = ["area"]
+        lst_org = [k for k,v in self.cds_filter.items() if isinstance(v, list) and k not in exclude_keys]
+        return lst_org
 
     def _get_request_size(self, lst_keys):
         request_size = math.prod(
@@ -117,6 +124,12 @@ class ClimateDataStoreDownloader(object):
         )
         return request_size
 
+    def _get_split_keys(self):
+        lst_org = self._get_org_keys()
+        lst_ret = list()
+        while self._get_request_size(lst_org) > self.cds_webapi["selection_limit"]:
+            lst_ret.append(lst_org.pop(0))
+        return lst_ret
 
     def _expand_by_keys(self, dct, lst_keys):
         tmp_dct = copy.deepcopy(dct)
@@ -131,3 +144,4 @@ class ClimateDataStoreDownloader(object):
             cds_filter,
             file_name
         )
+
--- cds_downloader/tests/test_downloader.py
+++ cds_downloader/tests/test_downloader.py
@@ -1,5 +1,7 @@
 import os
 import pytest
+import cdsapi
+import math
 
 from cds_downloader import ClimateDataStoreDownloader
 
@@ -10,7 +12,7 @@ def era5_downloader():
             {
                 "product_type": "reanalysis",
                 "format": "grib",
-                "variable": [ "2m_temperature","potential_evaporation"],
+                "variable": ["2m_temperature", "potential_evaporation"],
                 "year": ["1979", "1980"],
                 "month": ["01", "02"],
                 "day": ["01", "02"],
@@ -20,13 +22,47 @@ def era5_downloader():
     )
 
 
+def test_cds_webapi(era5_downloader):
+    assert len(era5_downloader.cds_webapi.keys()) == 36
+    assert len(era5_downloader.cds_webapi.get("form")) == 10
+
+
+def test_org_keys(era5_downloader):
+    org_keys = era5_downloader._get_org_keys()
+    assert org_keys == ["variable", "year", "month", "day", "time"]
+
+
 def test_request_size(era5_downloader):
-    req_size = era5_downloader._get_request_size(
-        ["year", "month", "day", "time", "variable"])
+    req_size = era5_downloader._get_request_size(["variable", "year", "month", "day", "time"])
     assert req_size == 32
 
-def test_split_key(era5_downloader):
-    era5_downloader.cds_webapi["selection_limit"] = 4
+
+@pytest.mark.parametrize(
+    "selection_limit, expected_size",
+    [(64, 32), (6, 4), (3, 2)]
+)
+def test_split_size(era5_downloader, selection_limit, expected_size):
+    era5_downloader.cds_webapi["selection_limit"] = selection_limit
+    org_keys = era5_downloader._get_org_keys()
     split_keys = era5_downloader._get_split_keys()
-    assert split_keys == ['year', 'month', 'day', 'time']
-    # assert split_keys == []
+
+    size_keys_request = [k for k in org_keys if k not in split_keys]
+    size_request = era5_downloader._get_request_size(size_keys_request)
+
+    assert size_request <= era5_downloader.cds_webapi["selection_limit"]
+    assert size_request == expected_size
+
+
+@pytest.mark.skipif(
+    ((os.environ.get('CDSAPI_URL') is None) and
+     (os.environ.get('CDSAPI_KEY') is None)),
+    reason="requires CDSAPI_KEY and CDSAPI_URL environment variables")
+@pytest.mark.parametrize("selection_limit, expected_files", [(64, 1), (6, 8), (3, 16)])
+def test_download(era5_downloader, tmp_path, selection_limit, expected_files):
+    era5_downloader.cds_webapi["selection_limit"] = selection_limit
+    tmpdir = tmp_path / "data"
+    tmpdir.mkdir()
+    all_processes = era5_downloader.get_data(tmpdir)
+    assert len(os.listdir(tmpdir)) == len(all_processes)
+    assert len(os.listdir(tmpdir)) == expected_files
+
