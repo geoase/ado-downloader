7e497a2f1b6cbe676c614eedfbd6bf1077d59812
Georg A. Seyerl <g.seyerl@geoase.eu>
2020-09-14 09:50:27 +0200

    Missing bracket

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../cds_downloader')
+sys.path.insert(0, os.path.abspath('../../cds_downloader'))
 
 # Configuration file for the Sphinx documentation builder.
 #
