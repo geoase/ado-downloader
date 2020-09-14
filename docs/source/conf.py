4824c31035b9f0d45da6a280a7b7da837668507e
Georg A. Seyerl <g.seyerl@geoase.eu>
2020-09-14 10:08:06 +0200

    Add rtd configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,3 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../cds_downloader'))
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -17,6 +13,10 @@ sys.path.insert(0, os.path.abspath('../../cds_downloader'))
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../..'))
 
 
 # -- Project information -----------------------------------------------------
